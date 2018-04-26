e32d18fafed9da88c57c28718711ddf3d5e39664
YHL <yunahanlin@users.noreply.github.com>
2018-04-26 11:24:59 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -26,3 +26,43 @@ export const formatDate = (str) => {
         return parseInt(time / 31536000000) + '年前'
     }
 }
+
+
+
+
+//POST传参序列化
+axios.interceptors.request.use((config) => {
+    if(config.method  === 'post'){
+        config.data = qs.stringify(config.data);
+    }
+    return config;
+},(error) =>{
+     _.toast("错误的传参", 'fail');
+    return Promise.reject(error);
+});
+
+//返回状态判断
+axios.interceptors.response.use((res) =>{
+    if(!res.data.success){
+        // _.toast(res.data.msg);
+        return Promise.reject(res);
+    }
+    return res;
+}, (error) => {
+    _.toast("网络异常", 'fail');
+    return Promise.reject(error);
+});
+
+export function fetch(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, params)
+            .then(response => {
+                resolve(response.data);
+            }, err => {
+                reject(err);
+            })
+            .catch((error) => {
+               reject(error)
+            })
+    })
+}
