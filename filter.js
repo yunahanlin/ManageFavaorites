81ddff59f619ebe599d2b12ba91d3460408072a3
YHL <yunahanlin@users.noreply.github.com>
2018-06-08 13:51:09 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -225,3 +225,17 @@ var nowDate = new Date();
   .getDate();
  var dateStr = year + "-" + month + "-" + day;
 
+//手机适配方案
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 37.5 * (clientWidth / 375) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
